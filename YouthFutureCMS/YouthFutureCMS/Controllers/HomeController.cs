abf496d866bba42ecd56d234011e7f2fdae8c44e
Michael Pontius <mapontius@yahoo.com>
2017-11-10 13:58:04 -0700

    Save contenteditable content to datebase.
    It works with onClick. Click the div and change the content and then hit the save button. If you click on multiple divs it will only save the last edited div. We’ll have to change that. Hope that makes sense. Tested and worked on Chrome and Edge but didn’t work with Firefox.

--- YouthFutureCMS/YouthFutureCMS/Controllers/HomeController.cs
+++ YouthFutureCMS/YouthFutureCMS/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
 using System.Data.Entity;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -46,6 +49,30 @@ namespace YouthFutureCMS.Controllers
 
             //pass the specific model to the view
             return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult updateContent(string contentName, string contentInfo)
+        {
+            SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["SystemDataContext"].ConnectionString);
+            try
+            {
+                //name = "Michael";
+                //city = "Clinton";
+                //country = "USA"; 
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("UPDATE Contents SET ContentInfo = @contentInfo WHERE contentName = @ContentName", conn);
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.AddWithValue("@contentInfo", contentInfo);
+                cmd.Parameters.AddWithValue("@contentName", contentName);
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                return Content("Success");
+            }
+            catch (Exception ex)
+            {
+                return Content("failure");
+            }
         }
     }
 }
