a2156537ff095e1ca1177bb5afcefc2da92a99ad
TomBowker <thomasbowker@mail.weber.edu>
2017-12-02 20:42:44 -0700

    Updated the code

--- YouthFutureCMS/YouthFutureCMS/Controllers/CustomErrorUseController.cs
+++ YouthFutureCMS/YouthFutureCMS/Controllers/CustomErrorUseController.cs
@@ -1,20 +1,16 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using YouthFutureCMS.Models;
 
 namespace YouthFutureCMS.Controllers
 {
-	public class CustomErrorUseController : Controller
+    public class CustomErrorUseController : Controller
     {
-		//
-		// GET : /CustomErrorUse/
-
-		[HandleError]
-		public ActionResult Index()
+        // GET: CustomErrorUse\
+        [HandleError]
+        public ActionResult Index()
         {
             return View();
         }
--- YouthFutureCMS/YouthFutureCMS/Controllers/ErrorController.cs
+++ YouthFutureCMS/YouthFutureCMS/Controllers/ErrorController.cs
@@ -1,18 +1,14 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using YouthFutureCMS.Models;
 
 namespace YouthFutureCMS.Controllers
 {
     public class ErrorController : Controller
     {
-        //
-        // GET : /Error/
-
+        // GET: Error
         public ActionResult Index()
         {
             return View();
