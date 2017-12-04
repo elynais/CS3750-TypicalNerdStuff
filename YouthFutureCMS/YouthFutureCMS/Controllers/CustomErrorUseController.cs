f429934607ef4a470af35289ff7b560f8a7e27a5
TomBowker <thomasbowker@mail.weber.edu>
2017-12-03 18:07:26 -0700

    This is the version that works

---
+++ YouthFutureCMS/YouthFutureCMS/Controllers/CustomErrorUseController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YouthFutureCMS.Controllers
+{
+    public class CustomErrorUseController : Controller
+    {
+        // GET: CustomErrorUse
+
+        [HandleError]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
